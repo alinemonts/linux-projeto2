91fbe86b3b199f382efdaf4398cdc880ee0fe4dd
root <root@ip-172-31-39-149.sa-east-1.compute.internal>
2022-08-10 20:49:29 +0000

    Apache v.1.0

---
+++ apache-webserver.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Atualizando o servidor...."
+
+apt-get update
+apt-get upgrade -y
+
+echo "Instalando Apache2...."
+
+apt-get install apache2 -y
+
+echo "Instalando unzip...."
+
+apt-get install unzip -y
+
+echo "Baixando arquivos da aplicação....."
+
+cp /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html
